34535d9a2b97d388863349f5afdccd7fa6d64ce2
Ben Lynch <bdlynch7@gmail.com>
2023-07-08 11:48:11 -0500

    show collection works

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,27 +1,37 @@
 console.log('***** Music Collection *****')
 
+// Array for music collection
 let collection = [];
-
-
 // create function to accept title, artist, year as args
 function addToCollection(title, artist, yearPublished) {
-// create new object <album> to contain above paramaters
-let album = {
-    title,
-    artist,
-    yearPublished 
-};
-// add created object to end of the <collection> array
-collection.push(album);
-// log last item pushed to the array to make sure it's what we want
-console.log(collection[collection.length - 1]);
-return album;
+    console.log('In function addToCollection');
+    // create new object <album> to contain above paramaters 
+    let album = {
+        title,
+        artist,
+        yearPublished
+    }
+    // add created object to end of the <collection> array
+    collection.push(album);    
+    // console log to double check
+    console.log('New album is:', album.title);
+    return album;
 }
+
 addToCollection('I like it when you sleep, for you are so beautiful yet so unaware of it', 'the 1975', 2016);
 addToCollection('Operation: Doomsday', 'MF DOOM', 2001);
-addToCollection('Only Built 4 Cuban Linx...', 'Raekwon', 1995);
+addToCollection('Being Funny in a Foreign Language', 'the 1975', 2023);
 addToCollection('1989', 'Taylor Swift', 2013);
 addToCollection('Starboy', 'The Weeknd', 2016);
 addToCollection('Illmatic', 'Nas', 1994);
 
 console.log(collection);
+
+function showCollection(array) {
+    console.log('The array length is:', array.length);
+    for (album of array) {
+        console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+    }
+}
+
+showCollection(collection);
